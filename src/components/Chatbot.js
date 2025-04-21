0c84bcf29ae452dcf57b155985fa42b3d90553eb
Anugrah <anugrah@Anugrahs-MacBook-Air.local>
2025-04-21 13:26:35 +0530

    Added Gemini and GPT as chat models

--- src/components/Chatbot.js
+++ src/components/Chatbot.js
@@ -143,7 +143,7 @@ function Chatbot() {
     setIsBotResponding(true);
 
     try {
-      const response = await fetch("https://rag-chatbot-web.shop/chat", {
+      const response = await fetch("http://127.0.0.1:8000/chat", {
         method: "POST",
         headers: {
           "Content-Type": "application/x-www-form-urlencoded",
