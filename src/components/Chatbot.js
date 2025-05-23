8b34e06c5a726a1c81fb8ef0ed53011d47e854c7
Anugrah <anugrah@Anugrahs-MacBook-Air.local>
2025-05-23 15:43:27 +0530

    updates

--- src/components/Chatbot.js
+++ src/components/Chatbot.js
@@ -146,7 +146,7 @@ function Chatbot() {
 
    
     try {
-      const response = await fetch("https://datalysis.rag-chatbot-web.shop/chat", {
+      const response = await fetch("http://127.0.0.1:8000/chat", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
