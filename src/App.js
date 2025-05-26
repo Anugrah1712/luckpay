94ad1572864c0ecc335efd0bb85eafdd606ff5c8
Anugrah <anugrah@Anugrahs-MacBook-Air.local>
2025-05-26 15:40:04 +0530

    updates

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Chatbot from "./components/Chatbot";
-import DeveloperConsole from "./components/DeveloperConsole"; // Import DeveloperConsole
+import { Chatbot } from 'rag-chatbot-ui-bajaj'
 import "./App.css";
 
 function App() {
@@ -9,13 +8,8 @@ function App() {
     <Router>
       <div className="app">
         <h1 className="welcome-text">Welcome to My Website</h1>
-        <Chatbot />
-
-        {/* Define Routes */}
-        <Routes>
-          <Route path="/developer-console" element={<DeveloperConsole />} />
-        </Routes>
       </div>
+      <Chatbot/>
     </Router>
   );
 }
