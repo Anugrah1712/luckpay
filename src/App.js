ad14033ef4eb8e67c39c95fb6973bb47190ef9ed
Anugrah <anugrah@Anugrahs-MacBook-Air.local>
2025-06-16 11:50:43 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Chatbot } from 'rag-chatbot-ui-bajaj'
+import { Chatbot } from 'rag-chatbot-ui-luckpay'
 import "./App.css";
 
 function App() {
   return (
     <Router>
       <div className="app">
-        <h1 className="welcome-text">Welcome to My Website</h1>
+        <h1 className="welcome-text">Demo Chatbot for LuckPay</h1>
       </div>
       <Chatbot/>
     </Router>
